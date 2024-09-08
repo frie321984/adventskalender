a121b8d2ecf0d5847d54982485d8daa72e3c26ff
Friederike Schertel <friederike@schertel.org>
2024-09-08 12:13:44 +0000

    copy from website

---
+++ src/calendar.js
@@ -0,0 +1,169 @@
+const DEBUG_FLAG = !!localStorage.getItem('fs.weihnachten.debug');
+if (DEBUG_FLAG) console.log('debugging ist ON')
+
+const once={once:true}
+const adventDoors = 'adventDoors';
+const overlay = "adventOverlay";
+const imagePath = "/weihnachten/images/";
+const loadingUrl = imagePath + '/loading.jpg';
+const doc = document;
+const arrayConstructor = Array;
+genDoorId = (day) => day > 0 && day < 32 ? "door" + day : undefined;
+byId = (id) => doc.getElementById(id)
+a = (d) => d.getElementsByTagName('a')[0]
+img = (el) => el.getElementsByTagName('img')[0]
+xmasimgs= {
+    1: '1.jpg',
+    2: '2.jpg',
+    3: '3.jpg',
+    4: '4.jpg',
+    5: '5.jpg',
+    6: '6.jpg',
+    7: '7.jpg',
+    8: '8.jpg',
+    9: '9.jpg',
+    10: '10.jpg',
+    11: '11.jpg',
+    12: '12.jpg',
+    13: '13.jpg',
+    14: '14.jpg',
+    15: '15.jpg',
+    16: '16.jpg',
+    17: '17.jpg',
+    18: '18.jpg',
+    19: '23.jpg',
+    20: '19.jpg',
+    21: '20.jpg',
+    22: '21.jpg',
+    23: '22.jpg',
+    24: '24.jpg'
+}
+imgPath = (d) => {
+    return imagePath + xmasimgs[day(d)];
+};
+day = (d) => a(d).text
+clearEventHandlers = (el) => el.outerHTML = el.outerHTML
+preload = (url) => {
+    let im = new Image()
+    im.src = url // this starts the preloading in the browser
+    return im // return for testability
+}
+allDoors = () => byId(adventDoors).getElementsByTagName('li')
+
+isDoorClickable = li => {
+	const now = new Date();
+	const d = day(li);
+	const dbgRule = d <= 24;
+	const defaultRule = now.getMonth() >= 11 && d <= now.getDate()
+	return DEBUG_FLAG?dbgRule:defaultRule
+}
+
+showMiniImage=(tag)=>{
+	const door = byId(genDoorId(tag))
+	const link = a(door)
+	link.classList.add('opened')
+	setTimeout(
+		() => link.style = "background-image: url('" + imgPath(door) + "'); background-size:cover;",
+		500
+	)
+}
+
+clickDoor = (clickedDoor)=>{
+	!DEBUG_FLAG||console.debug('click')
+	const tag = day(clickedDoor);
+	!DEBUG_FLAG||console.debug(tag)
+	let o = byId(overlay);
+
+	img(o).src=imgPath(clickedDoor)
+	o.classList.remove('invisible')
+
+	arrayConstructor.from(allDoors()).forEach(d => clearEventHandlers(d) )
+
+	showMiniImage(tag);
+	setTimeout(() => makeOverlayCloseable(), 500)
+}
+
+makeDoorsClickable = () => {
+	arrayConstructor.from(allDoors())
+		.filter(isDoorClickable)
+		.forEach(d => {
+			d.addEventListener('click', () => clickDoor(d), once)
+			d.addEventListener('keydown', (ev) => {
+				const k=ev.key.toUpperCase()
+				if ([' ','ENTER'].includes(k)) clickDoor(d)
+			})
+		})
+}
+closeOverlay = (_) => {
+	!DEBUG_FLAG||console.debug('close')
+	let el = byId(overlay);
+	el.classList.add('invisible')
+	img(el).src = loadingUrl
+	clearEventHandlers(el)
+	setTimeout(makeDoorsClickable, 200)
+}
+
+makeOverlayCloseable = () => {
+	byId(overlay).addEventListener('click', closeOverlay, once)
+	byId(overlay).addEventListener('pointerdown', closeOverlay, once)
+	img(byId(overlay)).addEventListener('keydown', (ev) => {
+		if ([' ', 'ENTER', 'X'].includes(ev.key.toUpperCase())) closeOverlay()
+	})
+}
+openAllDoors = () => !DEBUG_FLAG || arrayConstructor.from(allDoors())
+    .filter(isDoorClickable)
+    .forEach(li => showMiniImage(day(li)))
+
+setupDevHud = () => {
+    if (!DEBUG_FLAG) return;
+    if (!byId("dev")) return;
+
+    const openAllLink = doc.createElement("button")
+    openAllLink.textContent = "Open all"
+    openAllLink.addEventListener('click', () => openAllDoors(), once)
+    openAllLink.addEventListener('keydown', (ev) => {
+        const k = ev.key.toUpperCase()
+        if ([' ', 'ENTER'].includes(k)) openAllDoors()
+    })
+    byId("dev").appendChild(openAllLink)
+}
+
+setup=()=>{
+	img(byId(overlay)).tabIndex=0
+    for (let day = 1; day <= 24; day++) {
+		const door = doc.createElement("li");
+		byId(adventDoors).appendChild(door);
+		door.id = genDoorId(day);
+
+		const link = doc.createElement("a");
+		link.innerHTML += day;
+		link.tabIndex=0
+		door.appendChild(link);
+	}
+    setupDevHud()
+}
+
+function main() {
+    setup();
+    preload(loadingUrl)
+    doc.addEventListener('keydown', (ev) => {
+        if (ev.key.toUpperCase()==="ESCAPE")
+            closeOverlay()
+    })
+    closeOverlay()
+
+    arrayConstructor.from(allDoors())
+        .filter(d => !isDoorClickable(d))
+        .forEach(d => {
+            a(d).tabIndex=-1
+            d.classList.add('disabled')
+        })
+
+    makeDoorsClickable()
+
+    // preloading
+    arrayConstructor.from(allDoors())
+        .filter(isDoorClickable)
+        .reverse()
+        .forEach(li => preload(imgPath(li)))
+}
---
+++ src/calendar.test.js
@@ -0,0 +1,102 @@
+const calendar = require('./calendar');
+
+function givenSampleHtml(htmlString) {
+    return new DOMParser().parseFromString(htmlString, 'text/html').getRootNode();
+}
+
+function givenDocumentBody(html) {
+    document.body.innerHTML = html;
+}
+
+describe('calendar', () => {
+    describe('genDoorId', () => {
+        it('valid values 1-31', () => {
+            expect(genDoorId(1)).toBe('door1');
+            expect(genDoorId(31)).toBe('door31');
+        })
+        it('invalid values', () => {
+            expect(genDoorId(32)).toBeUndefined();
+            expect(genDoorId(0)).toBeUndefined();
+            expect(genDoorId(-1)).toBeUndefined();
+        })
+        it('syntax errors will not throw', () => {
+            expect(genDoorId()).toBeUndefined();
+        })
+    });
+
+    describe('byId', () => {
+        it('valid call', () => {
+            givenDocumentBody(`
+            <div id="validId"></div>
+        `);
+            expect(byId('validId')).toBeTruthy()
+        })
+    })
+
+    describe('a link', () => {
+        it('should get first link in an element', () => {
+            const htmlElement = givenSampleHtml("<div><a>1</a><a>2</a></div>");
+            expect(a(htmlElement)).toBeTruthy()
+            expect(a(htmlElement).innerHTML).toEqual("1")
+        })
+
+        it('should fail on invalid call', () => {
+            expect(() => a()).toThrow()
+            expect(() => a(undefined)).toThrow()
+            expect(() => a('string')).toThrow()
+            expect(() => a(1234)).toThrow()
+        })
+    })
+
+    describe('img', () => {
+        it('should get first picture in an element', () => {
+            const htmlElement = givenSampleHtml("<div><img src='1' /><img src='2' /></div>");
+            expect(img(htmlElement)).toBeTruthy()
+            expect(img(htmlElement).src).toEqual("1")
+        })
+
+        it('should fail on invalid call', () => {
+            expect(() => img()).toThrow()
+            expect(() => img(undefined)).toThrow()
+            expect(() => img('string')).toThrow()
+            expect(() => img(1234)).toThrow()
+        })
+    })
+
+    describe('day', () => {
+        it('how to find out day of an element', () => {
+            const htmlElement = givenSampleHtml("<div><a>this-is-the-day</a></div>");
+            expect(day(htmlElement)).toEqual("this-is-the-day")
+        })
+    })
+
+    describe('allDoors', () => {
+        it('valid call', () => {
+            givenDocumentBody("<ul id='adventDoors'><li>1</li><li>2</li><li>3</li><li>4</li></ul>");
+            expect(allDoors().length).toEqual(4)
+        })
+        it('no list items', () => {
+            givenDocumentBody("<ul id='adventDoors'></ul>");
+            expect(allDoors().length).toEqual(0)
+        })
+        it('id not found', () => {
+            givenDocumentBody("<ul id='someOtherId'><li>1</li></ul>");
+            expect(()=>allDoors()).toThrow()
+        })
+    })
+
+    describe('clearEventHandlers', () => {
+        it('valid call', () => {
+            let element = givenSampleHtml('')
+            element.outerHTML = '<button></button>'
+            clearEventHandlers(element)
+            expect(element.outerHTML).toEqual("<button></button>")
+        })
+    })
+    describe('preload', () => {
+        it('valid call', () => {
+            expect(preload('foo')).toBeTruthy()
+            expect(preload('imageName.jpg').src).toMatch(/imageName\.jpg$/)
+        })
+    })
+});
